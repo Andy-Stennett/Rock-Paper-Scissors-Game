09bc88dcb4ef6f8d897d2ad9d5b4306c42ccd06f
Andy-Stennett <83307633+Andy-Stennett@users.noreply.github.com>
2024-05-14 12:01:33 +0100

    First JS hurdle cleared, HTML writing issue sorted.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,33 @@
 // Waiting for the page to load before starting the scripts //
-
 document.addEventListener("DOMContentLoaded", function() {
-
-   
-    /*for(let buttons of buttons) {
+    let buttons = this.getElementsByTagName('button');
+    for(buttons of buttons) {
         buttons.addEventListener("click", function() {
-
+            console.log(this);
             if(this.getAttribute("data-type") === "reset") {
-                document.getElementById("player-choice").innerHTML = <i class="fa-solid fa-circle placeholder--p"></i>; 
+                document.getElementById("player-choice").innerHTML = `<i class="fa-solid fa-circle placeholder--p"></i>`;
                 alert("Game reset, choose your weapon to play.");
             } else {
                 let playerChoice = this.getAttribute("data-type");
-                
                 mainGame(playerChoice);
-            
             }
                 //let gameType = this.getAttribute("data-type");
-            //alert(`You clicked ${gameType}`);
+            //alert(You clicked ${gameType});
         });
     }
-
-}); */
-
-function mainGame() {
-    /*let playerChoice = playerChoice;
-
-    if(playerChoice === "rock") {
-        document.getElementById("player-choice").innerHTML = <i class="fa-solid fa-hand-back-fist btn--rock" ></i>
-    }
-    else if(playerChoice === "paper") {
-        document.getElementById("player-choice").innerHTML = <i class="fa-solid fa-hand-paper placeholder--p"></i>;
-    }
-    else if(playerChoice === "scissors") {
-        document.getElementById("player-choice").innerHTML = <i class="fa-solid fa-hand-scissors placeholder--p"></i>;
-    }
-    else {
-        alert("Invalid choice, please choose again.");*/
-    } 
-   
-    function rockFunction() {
-   
-        document.getElementById("#player-choice").innerHTML = `<i class="fa-solid fa-hand-back-fist btn--rock" ></i>`;
-    }
-
-    /*     let computerChoice = Math.random();
-        if(computerChoice < 0.34) {
-            computerChoice = "rock";
-        } else if(computerChoice <= 0.67) {
-            computerChoice = "paper";
-        } else {
-            computerChoice = "scissors";
-        } */
-
 });
+function mainGame(playerChoice) {
+        if(playerChoice === "rock") {
+            document.getElementById("player-choice").innerHTML = `<i class="fa-solid fa-hand-back-fist btn--rock" ></i>`;
+        }
+        else if(playerChoice === "paper") {
+            document.getElementById("player-choice").innerHTML = `<i class="fa-solid fa-hand-paper btn--paper" style="font-size:60px; color:rgb(240, 121, 24); rotate:90deg;" ></i>`;
+        }
+        else if(playerChoice === "scissors") {
+            document.getElementById("player-choice").innerHTML = `<i class="fa-solid fa-hand-scissors btn--scissors"></i>`;
+        }
+        else {
+            alert("Invalid choice, please choose again.");
+        }
+        
+};
